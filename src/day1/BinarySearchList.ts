554c668aee720484f845ee05e11b947eca9c8b65
Shivam Kadwade <83583221+shivam-kadwade@users.noreply.github.com>
2022-10-19 01:09:04 +0530

    added code for binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
-}
+     let high = haystack.length 
+     let low = 0
+      if(haystack[low] === needle || haystack[high] === needle){
+         return true; 
+        } 
+        do{
+             let middle = Math.floor(low + (high - low) / 2) 
+             if(haystack[middle] === needle){
+                 return true; 
+                } 
+                if(needle < haystack[middle]){
+                     high = middle; 
+                    } else{
+                         low = middle+1; 
+                        } 
+                    } while(low < high); 
+                    return false; 
+                }
